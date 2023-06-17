ffd4b9b733042baab4bee3409fc68ae9d582cb26
Blechlawine <marczinser@gmx.de>
2023-06-17 23:38:18 +0200

    chore: remove todo

--- uno.config.ts
+++ uno.config.ts
@@ -2,7 +2,6 @@ import presetBlechUi from "./src/presets/index";
 import presetUno from "@unocss/preset-uno";
 import { defineConfig } from "unocss";
 
-// TODO: rebalance colors
 export default defineConfig({
     presets: [presetUno(), presetBlechUi()],
 });
